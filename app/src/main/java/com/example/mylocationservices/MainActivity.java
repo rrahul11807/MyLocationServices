40ab1319447c6b0a8bc188b6d1b38c7152f3efbb
Rahul <rrahul.rajappan@gmail.com>
2019-06-08 14:54:45 +1200

    Added final variables

--- app/src/main/java/com/example/mylocationservices/MainActivity.java
+++ app/src/main/java/com/example/mylocationservices/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.mylocationservices;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView txtLatitude = (TextView)findViewById(R.id.txtLatitude);
+        final TextView txtLongitude = (TextView)findViewById(R.id.txtLongitude);
+
+        final Button btnGetLocation = (Button) findViewById(R.id.btnLocation);
+
+        final FusedLocationProviderClient client = LocationServices.getFusedLocationProviderClient(this);
     }
 }
