9180bc938f2d6175bc61acb2d613e19354678222
Rahul <rrahul.rajappan@gmail.com>
2019-06-08 15:44:32 +1200

    Added on click listener

--- app/src/main/java/com/example/mylocationservices/MainActivity.java
+++ app/src/main/java/com/example/mylocationservices/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.mylocationservices;
 
+import android.location.Location;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,5 +26,31 @@ public class MainActivity extends AppCompatActivity {
         final Button btnGetLocation = (Button) findViewById(R.id.btnLocation);
 
         final FusedLocationProviderClient client = LocationServices.getFusedLocationProviderClient(this);
+
+        btnGetLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    Task<Location> location = client.getLastLocation();
+
+                    //TODO don't do this!
+                    //double lat = location.getResult().getLatitude();
+                    location.addOnCompleteListener(new OnCompleteListener<Location>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Location> task) {
+
+                            txtLatitude.setText(Double.toString(task.getResult().getLatitude()));
+                            txtLongitude.setText(Double.toString(task.getResult().getLongitude()));
+                            System.err.println(task.getResult().getLatitude());
+                        }
+                    });
+                }
+                catch(SecurityException ex)
+                {
+                    ex.printStackTrace();
+                }
+            }
+        });
+
     }
 }
